910311f7f0afeb07be1d940e77bab3e696d4fbc3
Presto <yoohan95@gmail.com>
2019-11-07 23:30:09 +0900

    최초 커밋

---
+++ README.md
@@ -0,0 +1,10 @@
+# 이응이응 iOS
+
+**기기**
+
+- iOS 13
+- Swift 5.1
+- SwiftUI
+- Combine
+- Then
+
---
+++ gigi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gigi
+//
+//  Created by Presto on 2019/11/07.
+//  Copyright © 2019 ieungieung. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
